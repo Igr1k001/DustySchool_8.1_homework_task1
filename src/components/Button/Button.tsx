c83d8c840519eb483a9c3616c65958b950255f55
Игорь Поливода <igr1k@MacBook-Pro.local>
2024-02-27 14:55:37 +0300

    fix

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -13,18 +13,6 @@ export interface IButtonProps {
 }
 
 export const Button: React.FC<IButtonProps> = ({ theme, type, hover, disable }) => {
-    // const classNames = ['Button'];
-
-    // classNames.push(theme === 'light' ? 'Button_theme_light' : 'Button_theme_dark');
-    // classNames.push(type === 'primary' ? 'Button_type_primary' : 'Button_type_secondary');
-
-    // if (hover) {
-    //     classNames.push('Button_hover_yes');
-    // }
-
-    // if (disable) {
-    //     classNames.push('Button_disable_yes');
-    // }
 
     const classNames = cnButton({
         theme: theme === 'light' ? 'light' : 'dark',
