5fe5fb23bc44d7066e358380b596608b4ab57cdc
Игорь Поливода <igr1k@MacBook-Pro.local>
2024-02-27 14:54:20 +0300

    changes

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,59 +2,40 @@ import React from 'react';
 
 import './Button.css';
 
+import { cnButton } from './Button.classname';
+
+
 export interface IButtonProps {
     theme: 'dark' | 'light';
     type: 'primary' | 'secondary';
-    hover?: Boolean;
-    disable?: Boolean;
+    hover?: boolean;
+    disable?: boolean;
 }
 
 export const Button: React.FC<IButtonProps> = ({ theme, type, hover, disable }) => {
-    const classNames = ['Button'];
+    // const classNames = ['Button'];
 
-    classNames.push(theme === 'light' ? 'Button_theme_light' : 'Button_theme_dark');
-    classNames.push(type === 'primary' ? 'Button_type_primary' : 'Button_type_secondary');
+    // classNames.push(theme === 'light' ? 'Button_theme_light' : 'Button_theme_dark');
+    // classNames.push(type === 'primary' ? 'Button_type_primary' : 'Button_type_secondary');
 
-    if (hover) {
-        classNames.push('Button_hover_yes');
-    }
+    // if (hover) {
+    //     classNames.push('Button_hover_yes');
+    // }
 
-    if (disable) {
-        classNames.push('Button_disable_yes');
-    }
+    // if (disable) {
+    //     classNames.push('Button_disable_yes');
+    // }
+
+    const classNames = cnButton({
+        theme: theme === 'light' ? 'light' : 'dark',
+        type: type === 'primary' ? 'primary' : 'secondary',
+        hover: hover,
+        disable: disable
+    });
 
     return (
-        <button className={classNames.join(' ')}>
+        <button className={classNames}>
             Hello
         </button>
     );
 }
-
-
-
-
-
-// import React from 'react';
-
-// import './Button.css';
-
-// export interface IButtonProps extends React.PropsWithChildren {
-//     theme?: 'dark' | 'light';
-//     type?: 'primary' | 'secondary';
-//     condition?: 'normal' | 'hover' | 'disable';
-// }
-
-// export const Button: React.FC<IButtonProps> = (props) => {
-//     const { children, theme = 'light', type = 'primary', condition='normal' } = props;
-
-//     return (
-//         <button className={`
-//             Button 
-//             ${theme === 'light' ? 'Button_theme_light' : 'Button_theme_dark'} 
-//             ${type === 'primary' ? 'Button_type_primary' : 'Button_type_secondary'}
-//             ${condition === 'normal' ? 'Button_condition_normal' : 'Button_type_secondary'}
-//         `}>
-//             {children}
-//         </button>
-//     );
-// }
